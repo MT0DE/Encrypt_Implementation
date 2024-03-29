a39e55a7e073bd46de02c8e04335d6808b43a42c
Lukas Lannge <lannge@kth.se>
2024-03-29 12:25:25 +0100

    Update to python RSA

--- RSA_impl/py_encrypt_RSA.py
+++ RSA_impl/py_encrypt_RSA.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import time
+import glob
 
 from cryptography.hazmat.primitives.asymmetric import rsa, padding
 from cryptography.hazmat.primitives import serialization, hashes
@@ -23,21 +24,21 @@ def generate_keys():
         print('Public key generated')
 
 def load_pr_key():
-    pr_pem = open("private_key.pem", "rb").read()
+    pr_pem = open("4096_private_key.pem", "rb").read()
     pr_key = serialization.load_pem_private_key(pr_pem, None)
     return pr_key
 
 
 def load_pu_key():
-    pu_pem = open("public_key.pem", "rb").read()
+    pu_pem = open("4096_public_key.pem", "rb").read()
     pu_key = serialization.load_pem_public_key(pu_pem, None)
     return pu_key
 
-def encrypt_message(plaintext):
-    plaintext = plaintext.encode()
+def encrypt_message(plaintext: str, key: rsa.RSAPrivateKey):
+    plaintext_bytes = plaintext.encode()
     pu_key = load_pu_key()
     ciphertext = pu_key.encrypt(
-        plaintext, 
+        plaintext_bytes, 
         padding.OAEP(
             padding.MGF1(hashes.SHA256()), 
             hashes.SHA256(), 
@@ -46,7 +47,7 @@ def encrypt_message(plaintext):
     )
     return ciphertext
 
-def decrypt_message(ciphertext):
+def decrypt_message(ciphertext: str, key: rsa.RSAPrivateKey):
     pr_key = load_pr_key()
     plaintext = pr_key.decrypt(
         ciphertext, 
@@ -61,38 +62,68 @@ def decrypt_message(ciphertext):
 def load_files_fifo():
    print("loading files into memory")
    # print(f"I am at {os.getcwd()}")
+   
+   # Save current directory and change to select directory
    curr_dir = os.getcwd()
-   os.chdir('../AES_plaintext_files')
+   os.chdir('../plaintext_files/RSA')
    # print(f"I am now at {os.getcwd()}")
 
    data_from_files = []
-   for _, _, files in os.walk(os.getcwd()):
-      # print(files)
-      for filename in files:
-         # print(filename)
-         with open(os.path.join(os.curdir, filename), 'r') as df:
-            data_from_files.append(df.read().encode())
+   
+   #Select all .txt files    
+   txtfiles = [f for f in glob.glob("*.txt")]
+   for filename in txtfiles:
+        # print(filename)
+        # Open the file as binary file and read all of its content
+        with open(os.path.join(os.curdir, filename), 'rb') as df:
+            data_from_files.append(df.read())
+   
+   #Return to original directory
    os.chdir(curr_dir)
    return data_from_files
 
+def load_keys_fifo():
+    print("loading key_files into memory")
+    keys = []
+    pr_pem = open("1024_private_key.pem", "rb").read()
+    pr_key_1024 = serialization.load_pem_private_key(pr_pem, None)
+    keys.append(pr_key_1024)
+
+    pr_pem = open("2048_private_key.pem", "rb").read()
+    pr_key_1024 = serialization.load_pem_private_key(pr_pem, None)
+    keys.append(pr_key_1024)
+
+    pr_pem = open("3072_private_key.pem", "rb").read()
+    pr_key_1024 = serialization.load_pem_private_key(pr_pem, None)
+    keys.append(pr_key_1024)
+
+    pr_pem = open("4096_private_key.pem", "rb").read()
+    pr_key_1024 = serialization.load_pem_private_key(pr_pem, None)
+    keys.append(pr_key_1024)
+
+    return keys
+
+
 def benchmark():
-   data = load_files_fifo()
-   acc_time = 0
-   iterations = 500
-   text: str = ""
-   print(f"Encrypting with RSA {iterations} times")
-   for _ in range(11):
-      text = data.pop()
-      size_of_text = sys.getsizeof(text)
-      for x in range(iterations):
-         print("\r" + f'   [{x+1}] filesize {size_of_text} bytes', end='')
-         time_x1 = time.perf_counter()
-         encrypt_message(text.encode())
-         time_x2 = time.perf_counter()
-         acc_time += time_x2 - time_x1
-      tot_time = acc_time * 1000 / (iterations)
-      print(f'    {tot_time:.3f} ms')
-      acc_time = 0
+    data = load_files_fifo()
+    keys = load_keys_fifo()
+    acc_time = 0
+    iterations = 500
+    text: str = ""
+    print(f"Encrypting with RSA {iterations} times")
+    for _ in range(4):
+        text = data.pop()
+        key = rsa.RSAPrivateKey(keys.pop())
+        size_of_text = sys.getsizeof(text)
+        for x in range(iterations):
+            print("\r" + f'   [{x+1}] filesize {size_of_text} bytes', end='')
+            time_x1 = time.perf_counter()
+            encrypt_message(text)
+            time_x2 = time.perf_counter()
+            acc_time += time_x2 - time_x1
+        tot_time = acc_time * 1000 / (iterations)
+        print(f'    {tot_time:.3f} ms')
+        acc_time = 0
 
 
 

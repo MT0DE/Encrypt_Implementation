c7b42ee8a5bda5a32513057f4cf75dfaf6f35b00
Samuel Sendek <cooperavalanche@gmail.com>
2024-03-29 10:28:15 +0100

    Update py_encrypt_RSA.py

--- RSA_impl/py_encrypt_RSA.py
+++ RSA_impl/py_encrypt_RSA.py
@@ -1,6 +1,7 @@
 import os
+import sys
 import time
-import timeit
+
 from cryptography.hazmat.primitives.asymmetric import rsa, padding
 from cryptography.hazmat.primitives import serialization, hashes
 
@@ -33,6 +34,7 @@ def load_pu_key():
     return pu_key
 
 def encrypt_message(plaintext):
+    plaintext = plaintext.encode()
     pu_key = load_pu_key()
     ciphertext = pu_key.encrypt(
         plaintext, 
@@ -56,8 +58,42 @@ def decrypt_message(ciphertext):
     )
     return plaintext
 
+# def load_files_fifo():
+#    print("loading files into memory")
+#    # print(f"I am at {os.getcwd()}")
+#    curr_dir = os.getcwd()
+#    os.chdir('../plaintext_files')
+#    # print(f"I am now at {os.getcwd()}")
 
-ct = encrypt_message(TEXT.encode())
+#    data_from_files = []
+#    for _, _, files in os.walk(os.getcwd()):
+#       # print(files)
+#       for filename in files:
+#          # print(filename)
+#          with open(os.path.join(os.curdir, filename), 'r') as df:
+#             data_from_files.append(df.read())
+#    os.chdir(curr_dir)
+#    return data_from_files
+
+# def benchmark():
+#     data = load_files_fifo()
+#     acc_time = 0
+#     iterations = 250
+#     text = ""
+#     print(f"Encrypting with RSA {iterations} times")
+#     for _ in range(11):
+#         text = data.pop()
+#         for _ in range(iterations):
+#             time_x1 = time.perf_counter()
+#             ct = encrypt_message(text)
+#             time_x2 = time.perf_counter()
+#             acc_time += time_x2 - time_x1
+#         tot_time = acc_time * 1000 / (iterations)
+#         print(f'    filesize {sys.getsizeof(text)} bytes: {tot_time:.3f} ms')
+#         acc_time = 0
+
+# benchmark()
+ct = encrypt_message(TEXT)
 print("Encrypted message:", ct)
 pt = decrypt_message(ct)
 print("Decrypted message:", pt)

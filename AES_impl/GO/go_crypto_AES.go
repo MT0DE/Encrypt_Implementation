b7b29511fb7166fb4f6ce14b0d7c1044087a6d17
Lukas Lannge <lannge@kth.se>
2024-04-01 19:05:48 +0200

    Crated GO file and key generation function

---
+++ AES_impl/GO/go_crypto_AES.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"bytes"
+	"errors"
+	// "crypto/aes"
+	"bufio"
+	"crypto/rand"
+	"fmt"
+	"os"
+)
+
+//key size: 128bit
+const KEY_SIZE_IN_BYTES = 16
+
+func generate_key_and_save_to(filename string){
+	// create the 128bit key for AES128
+	byte_arr := make([]byte, KEY_SIZE_IN_BYTES)
+
+	//reads random bytes into byte_arr
+	read_bytes, err := rand.Read(byte_arr)
+	if read_bytes != len(byte_arr){
+		fmt.Println("Not enough bytes were read\nSpecific issue: ", err)
+		os.Exit(1)
+	}
+	fmt.Println("KeyGeneratorLog: Generated", KEY_SIZE_IN_BYTES, "cryptographically secure bytes")
+	
+	//create file and write the key to it
+	//from https://stackoverflow.com/questions/12518876/how-to-check-if-a-file-exists-in-go
+	if _, err := os.Stat(filename); errors.Is(err, os.ErrNotExist){
+		fmt.Println("File does not exist, but will be created")
+	}else{
+		fmt.Println("Files exist, but its contents will be truncated/removed")
+	}
+	outputfile, err := os.Create(filename)
+	if(err != nil){
+		fmt.Println(err)
+		os.Exit(1)
+	} else{
+		fmt.Printf("KeyGeneratorLog: Opened \"%s\" for writing\n", filename)
+	}
+
+	//defer will execute the supplied function BEFORE the fucntions exists
+	defer outputfile.Close()
+
+	writer := bufio.NewWriter(outputfile)
+	defer writer.Flush()
+	bytes_written, err := writer.Write(byte_arr)
+	if err != nil{
+		fmt.Println(err, "\nOnly", bytes_written, "were written to file")
+	}
+	fmt.Println("Buffer has", writer.Buffered(), "inside of it")
+	fmt.Printf("KeyGeneratorLog: \"%s\" was succesfully created and wrote %d bytes\n", filename, bytes_written)
+}
+
+func encrypt(text bytes.Buffer){
+	
+}
+
+func main() {
+	filename := "secret.key"
+	generate_key_and_save_to(filename)
+	
+}
